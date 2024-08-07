64d0905828941b469e73acd8a6d11b47fc078a28
Le Que Tran <ledangquetran126@gmail.com>
2024-08-07 15:29:20 +0700

    KT

---
+++ Cau1.10.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char ten[] = "Le Thi Yen Nhi";
+    if (strstr(ten, "Van") != NULL) {
+        printf("Giới tính: Nam\n");
+    }
+    else if (strstr(ten, "Thi") != NULL) {
+        printf("Giới tính: Nu\n");
+    }
+    else {
+        printf("Không thể xác định giới tính.\n");
+    }
+
+    getchar();
+
+    return 0;
+}
