b490c64b3a8054bdaa3d01d6220e5ea745472f3d
Le Que Tran <ledangquetran126@gmail.com>
2024-08-07 14:50:01 +0700

    KT

---
+++ Cau1.3.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char ten[] = "Le Dang Que Tran";
+    int kytuHoa = 0;
+
+    for (int i = 0; ten[i] != '\0'; i++) {
+        if (isupper(ten[i])) {
+            kytuHoa = 1;
+            break;
+        }
+    }
+
+    if (kytuHoa) {
+        printf("Chuỗi chứa ký tự hoa.\n");
+    }
+    else {
+        printf("Chuỗi không chứa ký tự hoa.\n");
+    }
+
+    getchar();
+
+    return 0;
+}
