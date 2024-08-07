5a0195f9b7ee969750cd9eb2cb5bfb5e54401309
Le Que Tran <ledangquetran126@gmail.com>
2024-08-07 15:03:37 +0700

    KT

---
+++ Cau1.5.cpp
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    char ten[] = "Le Dang Que Tran";  
+    char ch;                         
+    bool found = false;           
+
+    
+    printf("Nhap ky tu de kiem tra: ");
+    scanf_s(" %c", &ch);  
+
+    
+
+
+   
+    for (int i = 0; ten[i] != '\0'; i++) {
+        if (ten[i] == ch) {
+            found = true;
+            break;
+        }
+    }
+
+    
+    if (found) {
+        printf("Ky tu '%c' co trong chuoi.\n", ch);
+    }
+    else {
+        printf("Ky tu '%c' khong co trong chuoi.\n", ch);
+    }
+
+    
+    printf("Nhan Enter de thoat...");
+    getchar();  
+    getchar();
+
+    return 0;
+}
