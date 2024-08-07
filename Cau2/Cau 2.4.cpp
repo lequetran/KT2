c5c818a83c0703c9f42f53ab058b543187c9e49e
Le Que Tran <ledangquetran126@gmail.com>
2024-08-07 15:51:36 +0700

    KT

---
+++ Cau2/Cau 2.4.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+
+int fibonacci(int n) {
+    if (n <= 1) return n; 
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int n = 5;
+    int result = fibonacci(n);
+    printf("Số hạng thứ %d của dãy Fibonacci là %d\n", n, result);
+    getchar();
+    return 0;
+}
