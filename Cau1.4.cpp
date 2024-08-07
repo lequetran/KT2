9f8fa72ed7d10a096f4ba2871a2158341c6030eb
Le Que Tran <ledangquetran126@gmail.com>
2024-08-07 14:55:30 +0700

    KT

---
+++ Cau1.4.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char ten[] = "Le Dang Que Tran";
+    int demsoTu = 0;
+    int tu = 0;
+
+    for (int i = 0; ten[i] != '\0'; i++) {
+        if (isspace(ten[i])) {
+            tu = 0;
+        }
+        else {
+A            if (!tu) {
+                demsoTu++;
+            }
+            tu = 1;
+        }
+    }
+
+    printf("So tu trong chuoi: %d\n", demsoTu);
+
+    getchar();
+
+    return 0;
+}
