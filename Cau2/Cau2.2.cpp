0a0be016133835a22df3f3351efa64a98b3903ae
Le Que Tran <ledangquetran126@gmail.com>
2024-08-07 15:43:04 +0700

    KT

---
+++ Cau2/Cau2.2.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+
+int arraySum(int arr[], int n) {
+    if (n <= 0) return 0; 
+    return arraySum(arr, n - 1) + arr[n - 1];
+}
+
+int main() {
+    int arr[] = { 1, 4, 3, 4, 5 };
+    int n = 5;
+    int result = arraySum(arr, n);
+    printf("Tổng mảng là %d\n", result);
+    getchar();
+    return 0;
+}
